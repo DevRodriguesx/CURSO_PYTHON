b39edfd1db500a378a470a6b8dc9097391a918e3
DevRodriguesx <programadorrodriguess@gmail.com>
2026-05-15 21:54:24 -0300

    Aula 1 do curso de python / Comentário e DocString

---
+++ aula1.py
@@ -0,0 +1,11 @@
+"""
+DocString
+- Posso escrever o que eu quiser em varias linhas
+- Basta usar as aspas triplas
+
+"""
+
+# Permite escrever um comentario em uma linha
+print('Hello world') # Na frente
+# Abaixo
+print("234")
