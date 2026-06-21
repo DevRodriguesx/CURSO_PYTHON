a11d0cafe82902a38b90c79fb98fe62221f0c7b0
DevRodriguesx <programadorrodriguess@gmail.com>
2026-06-21 19:30:25 -0300

    Exercício: variáveis ​​e tipos de dados para treino

---
+++ aula8.py
@@ -0,0 +1,11 @@
+# Exercício: variáveis ​​e tipos de dados para treino
+
+nome = str("José Pedro")
+sobrenome = str("Piovezan Rodrigues")
+idade = int(19)
+ano_nascimento = int(2007)
+maior_de_idade = bool(idade >= 18)
+altura_metros = float(1.64)
+
+print(f"Meu nome é {nome} e meu sobrenome é {sobrenome}. Tenho {idade} anos, nasci em {ano_nascimento} e minha altura é {altura_metros} metros.")
+print(f"É verdade que sou maior de idade? {maior_de_idade}")
